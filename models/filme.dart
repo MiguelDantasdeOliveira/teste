b8a59c6d89dea9e02e576b6687e1e171a6774b49
MiguelDantasdeOliveira <164576635+MiguelDantasdeOliveira@users.noreply.github.com>
2025-06-02 19:40:45 -0300

    Create filme.dart

---
+++ models/filme.dart
@@ -0,0 +1,11 @@
+class Filme {
+  int? id;
+  String titulo;
+  String urlImagem;
+  String genero;
+  String faixaEtaria;
+  String duracao;
+  double pontuacao;
+  String descricao;
+  int ano;
+}
