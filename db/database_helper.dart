948d0bb7a7c7bf08c4be64c9e17e30024e76525d
MiguelDantasdeOliveira <164576635+MiguelDantasdeOliveira@users.noreply.github.com>
2025-06-02 19:38:30 -0300

    Update database_helper.dart

--- db/database_helper.dart
+++ db/database_helper.dart
@@ -2,4 +2,23 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import '../models/filme.dart';
 
-class DatabaseHelper {}
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  static Database? _db;
+
+  DatabaseHelper._internal();
+
+  Future<Database> get db async {
+    if (_db != null) return _db!;
+    _db = await _initDb();
+    return _db!;
+  }
+
+  Future<Database> _initDb() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'filmes.db');
+
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+}
